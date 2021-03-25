7e9bd9dfbed6531e88e10546e52ed9e84ccefd46
Boris Hasanov <borisbrother@gmail.com>
2021-03-25 10:53:26 +0500

    Modify main.js file

--- js/main.js
+++ js/main.js
@@ -16,23 +16,52 @@
 
 // logging((a, b) => a * b)(4, 5);
 
-const count = fn => {
-  let counter = 0;
-  return (a, b) => {
-    counter += 1;
-    console.log(`Вызов #${counter} функции ${fn.name}`);
-    return fn(a, b);
-  };
-};
+// const count = fn => {
+//   let counter = 0;
+//   return (a, b) => {
+//     counter += 1;
+//     console.log(`Вызов #${counter} функции ${fn.name}`);
+//     return fn(a, b);
+//   };
+// };
 
-const sum = (a, b) => a + b;
-const mult = (a, b) => a * b;
+// const sum = (a, b) => a + b;
+// const mult = (a, b) => a * b;
 
-const countsum = count(sum);
-const countmulti = count(mult);
+// const countsum = count(sum);
+// const countmulti = count(mult);
 
-const a = countsum(2, 3);
-const b = countsum(5, 6);
-const c = countmulti(5, 6);
-const d = countmulti(6, 6);
-console.dir({ a, b, c, d });
+// const a = countsum(2, 3);
+// const b = countsum(5, 6);
+// const c = countmulti(5, 6);
+// const d = countmulti(6, 6);
+// console.dir({ a, b, c, d });
+
+// const add = (a, b) => a + b;
+// const sum = (a, b, cb) => cb(a + b);
+
+// sum(5, 4, console.log.bind(null, "Use sum:"));
+
+// const fs = require("fs");
+
+// fs.readFile("./main.js", "utf8", (err, data) => {
+//   if (!err) {
+//     console.log({ lines: data.split("\n").length });
+//   } else {
+//     console.log(err.message);
+//   }
+// });
+// console.log("end");
+
+// const print = (fileName, err, data) => {
+//   if (!err) {
+//     console.log({ lines: data.split("\n").length, filename: fileName });
+//   } else {
+//     console.log(err.message);
+//   }
+// };
+
+// const path = "./main.js";
+// const cb = print.bind(null, path);
+// fs.readFile(path, "utf8", cb);
+console.log(document.cookie);
